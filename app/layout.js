dd079a270009c612d432b4bcceabeccdf9010f3d
itzSerag <serag.eldien.mahmoud@gmail.com>
2024-04-26 23:42:08 +0300

    google verification

--- app/layout.js
+++ app/layout.js
@@ -17,9 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
       <meta name="google-site-verification" content="IJmxHYDpir-qXYB7SshaYEpjbtGSIjDBL5MtQzEqyYY" />
-      </Head>
       <body className={inter.className}>
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
