2805fc408afe24dcf56aba519b33d53a54722aee
itzSerag <serag.eldien.mahmoud@gmail.com>
2024-04-26 23:33:28 +0300

    .

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ import Footer from './components/footer';
 import Navbar from './components/navbar';
 import './css/card.scss';
 import './css/globals.scss';
+import Head  from 'next/head';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
@@ -16,6 +17,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+      <meta name="google-site-verification" content="IJmxHYDpir-qXYB7SshaYEpjbtGSIjDBL5MtQzEqyYY" />
+      </Head>
       <body className={inter.className}>
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
