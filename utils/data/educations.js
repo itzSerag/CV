9e6d05dfa52c6ba62bc936455fabdadef5c1acd0
itzSerag <serag.eldien.mahmoud@gmail.com>
2024-04-27 14:36:04 +0300

    jsut bachalore education

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -4,12 +4,6 @@ export const educations = [
     title: "Bachelor Degree",
     duration: "2020 - Present",
     institution: "Ain Shams University - Scinece Faculty / Computer Science Department",
-  },
-  {
-    id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "El Tabary International School , Roxi [T.I.S]",
-  },
+  }
 
 ]
