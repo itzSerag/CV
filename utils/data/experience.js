42632997d2b76641453ccf234230fe52075d7903
itzSerag <serag.eldien.mahmoud@gmail.com>
2024-10-13 10:30:06 +0300

    editing freelance title

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -3,18 +3,17 @@ export const experiences = [
     id: 1,
     title: "AI Internship",
     company: "Imapct",
-    duration: "(Jan 2023 - Aug 2023)",
+    duration: "(Jan 2023 - Sep 2023)",
   },
   {
     id: 2,
-    title: "FullStack Developer Internship",
-    company: "CodSoft",
-    duration: "(Sep 2023 - Nov 2023)",
+    title: "Microsoft Student Partner",
+    company: "MSP Cairo University",
+    duration: "(Oct 2021 - Dec 2021)",
   },
   {
     id: 3,
-    title:
-      "Developed and deployed 2 web applications, integrating third-party services like Paymob for payment processing and AWS S3 for cloud storage",
+    title: "Freelance Web Developer",
     company: "Remote",
     duration: "(Jan 2024 - Present)",
   },
