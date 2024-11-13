0aa68e4827c17afa72e80d940190ec0d42120f82
itzSerag <serag.eldien.mahmoud@gmail.com>
2024-11-13 15:49:56 +0200

    updating resume on personal data

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -15,5 +15,5 @@ export const personalData = {
   leetcode: "https://leetcode.com/u/user8486Mh",
   devUsername: "itzserag",
   resume:
-    "https://drive.google.com/file/d/1U1ciRqj-20OkSHH4kVuF1FODRzjo6aUS/view?usp=sharing",
+    "https://drive.google.com/file/d/1tor-L3dLWPXall6OEXL6SZhvV9lqc9Ep/view?usp=sharing",
 };
