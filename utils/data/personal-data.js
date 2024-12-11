48e56c3a91a097ed491afa9721bdf90a640a97e9
itzSerag <serag.eldien.mahmoud@gmail.com>
2024-12-11 22:16:28 +0200

    update leetCode URL

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,7 +1,7 @@
 export const personalData = {
   name: "Serag Eldien Mahmoud",
   profile: "/profile.png",
-  designation: "Software Developer Backend NodeJs",
+  designation: "Software Developer Backend NodeJs / NestJs",
   description:
     "I'm Serag, a passionate programmer fueled by curiosity and driven by challenges. Specializing in Backend, I thrive on crafting innovative solutions for web development projects. Eager to explore new technologies and make a meaningful impact in the digital world, I'm ready to take on any opportunity that aligns with my skills and interests.",
   email: "serag.eldien.mahmoud@gmail.com",
@@ -12,7 +12,7 @@ export const personalData = {
   linkedIn: "https://www.linkedin.com/in/serag-eldien-mahmoud-530a51233/",
   twitter: "",
   stackOverflow: "",
-  leetcode: "https://leetcode.com/u/user8486Mh",
+  leetcode: "https://leetcode.com/u/serag_eldien_mahmoud",
   devUsername: "itzserag",
   resume:
     "https://drive.google.com/file/d/1MkW-hSZuagudwn03SPv9Y-ynFCj8Wgwu/view?usp=sharing",
